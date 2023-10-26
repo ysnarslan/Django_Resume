eb2f69d8505d1db1aa951bd1f5043b554caff614
Yasin Arslan <ysnarsln1730@gmail.com>
2023-10-26 17:35:42 +0300

    feat: create image setting model

--- core/admin.py
+++ core/admin.py
@@ -5,9 +5,20 @@ from .models import *
 # Register your models here.
 
 @admin.register(GeneralSetting)
-class GeneralSettingsAdmin(admin.ModelAdmin):
-    list_display = ['name', 'description', 'parameter', 'updated_date', 'created_date']
+class GeneralSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'parameter', 'updated_date', 'created_date']
     search_fields = ['name', 'description', 'parameter']
     list_editable = ['description', 'parameter']
+
     class Meta:
         model = GeneralSetting
+
+
+@admin.register(ImageSetting)
+class ImageSettingAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name', 'description', 'file', 'updated_date', 'created_date']
+    search_fields = ['name', 'description', 'file']
+    list_editable = ['description', 'file']
+
+    class Meta:
+        model = ImageSetting
--- core/models.py
+++ core/models.py
@@ -42,3 +42,45 @@ class GeneralSetting(models.Model):
         verbose_name = 'General Setting'
         verbose_name_plural = 'General Settings'
         ordering = ('name',)
+
+
+class ImageSetting(models.Model):
+    name = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name='Name',
+        help_text='This is variable of user name.',
+    )
+    description = models.CharField(
+        default='',
+        max_length=255,
+        blank=True,
+        verbose_name='Description',
+        help_text='',
+    )
+    file = models.ImageField(
+        default='',
+        blank=True,
+        verbose_name='Image',
+        help_text='',
+        upload_to='images/',
+    )
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated Date',
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date',
+    )
+
+    def __str__(self):
+        return f'Image Setting: {self.name}'
+
+    class Meta:
+        verbose_name = 'Image Setting'
+        verbose_name_plural = 'Image Settings'
+        ordering = ('name',)
