b3eb99fadd5465a85ba785f22ab1d12ce0026b5b
Yasin Arslan <ysnarsln1730@gmail.com>
2023-10-26 18:54:35 +0300

    refactor: create abstract model

--- core/models.py
+++ core/models.py
@@ -1,8 +1,24 @@
 from django.db import models
 
 
+class AbstractModel(models.Model):
+    updated_date = models.DateTimeField(
+        blank=True,
+        auto_now=True,
+        verbose_name='Updated Date',
+    )
+    created_date = models.DateTimeField(
+        blank=True,
+        auto_now_add=True,
+        verbose_name='Created Date',
+    )
+
+    class Meta:
+        abstract = True
+
+
 # Create your models here.
-class GeneralSetting(models.Model):
+class GeneralSetting(AbstractModel):
     name = models.CharField(
         default='',
         max_length=255,
@@ -24,16 +40,6 @@ class GeneralSetting(models.Model):
         verbose_name='Parameter',
         help_text='',
     )
-    updated_date = models.DateTimeField(
-        blank=True,
-        auto_now=True,
-        verbose_name='Updated Date',
-    )
-    created_date = models.DateTimeField(
-        blank=True,
-        auto_now_add=True,
-        verbose_name='Created Date',
-    )
 
     def __str__(self):
         return f'General Setting: {self.name}'
@@ -44,7 +50,7 @@ class GeneralSetting(models.Model):
         ordering = ('name',)
 
 
-class ImageSetting(models.Model):
+class ImageSetting(AbstractModel):
     name = models.CharField(
         default='',
         max_length=255,
@@ -66,16 +72,7 @@ class ImageSetting(models.Model):
         help_text='',
         upload_to='images/',
     )
-    updated_date = models.DateTimeField(
-        blank=True,
-        auto_now=True,
-        verbose_name='Updated Date',
-    )
-    created_date = models.DateTimeField(
-        blank=True,
-        auto_now_add=True,
-        verbose_name='Created Date',
-    )
+
 
     def __str__(self):
         return f'Image Setting: {self.name}'
