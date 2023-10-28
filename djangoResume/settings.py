aa3b4da6659eb258d867dbaceb8441848b568afc
Yasin Arslan <ysnarsln1730@gmail.com>
2023-10-28 17:58:06 +0300

    feat: create postgres database in docker

--- djangoResume/settings.py
+++ djangoResume/settings.py
@@ -82,10 +82,7 @@ WSGI_APPLICATION = 'djangoResume.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': env.db()
 }
 
 # Password validation
